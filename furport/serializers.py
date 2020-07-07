ee94255ff214efdee83427f217cd16d469c0f043
Lapi <tyoutugai.7@gmail.com>
2020-07-08 06:20:08 +0900

    add user create

--- furport/serializers.py
+++ furport/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
+
 from furport.models import Event, Tag
 
 
@@ -16,3 +18,16 @@ class TagSerializer(serializers.HyperlinkedModelSerializer):
         model = Tag
         fields = "__all__"
         read_only_fields = ("created_at", "updated_at")
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ["email", "username", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        user = User(email=validated_data["email"], username=validated_data["username"])
+        user.set_password(validated_data["password"])
+        user.save()
+        return user
--- furport/views.py
+++ furport/views.py
@@ -1,10 +1,12 @@
-from rest_framework import permissions, renderers, viewsets
+from rest_framework import permissions, renderers, viewsets, mixins
 from rest_framework.decorators import action
+from django.contrib.auth.models import User
 
 from furport.models import Event, Tag
 from furport.serializers import (
     EventSerializer,
     TagSerializer,
+    UserSerializer,
 )
 from furport.permissions import IsOwnerOrReadOnly
 
@@ -26,3 +28,9 @@ class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+
+class UserViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
+    queryset = User.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = UserSerializer
