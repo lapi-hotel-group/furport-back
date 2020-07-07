e3a623726cbbc19e03cd44f29b71a41a3fe468cf
Lapi <tyoutugai.7@gmail.com>
2020-07-08 03:00:36 +0900

    add django-rest-framewolk-jwt

--- djangoapp/settings.py
+++ djangoapp/settings.py
@@ -122,4 +122,10 @@ STATIC_URL = "/static/"
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_jwt.authentication.JSONWebTokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
+    ),
 }
