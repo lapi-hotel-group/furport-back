70f842dfde1bb5367769cf8165493628f583022a
Lapi <tyoutugai.7@gmail.com>
2020-07-08 20:50:23 +0900

    add user profile

--- furport/serializers.py
+++ furport/serializers.py
@@ -1,13 +1,19 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Permission
 
-from furport.models import Event, Tag
+from furport.models import Event, Tag, Profile
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ["url", "username"]
+        fields = ("username", "url")
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = "__all__"
 
 
 class EventSerializer(serializers.HyperlinkedModelSerializer):
